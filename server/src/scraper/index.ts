124aa5c0add5af2bca062d86a284105c77371c62
EJ Mercado <justinemmanuelmercado@gmail.com>
2021-01-25 17:31:08 +0800

    feat: add pia to scrapers

--- server/src/scraper/index.ts
+++ server/src/scraper/index.ts
@@ -1,15 +1,25 @@
 import { Inquirer } from "./source/Inquirer";
-import { Herald } from './source/Herald';
+import { Herald } from "./source/Herald";
 import { Supabase } from "./supabase";
 import { puppeteerHandler } from "./puppeteer";
-import { Sunstar } from './source/Sunstar';
+import { Sunstar } from "./source/Sunstar";
+import { Abscbn } from "./source/Abscbn";
+import { Pia } from "./source/Pia";
 
 (async () => {
   const inq = new Inquirer(puppeteerHandler);
   const he = new Herald(puppeteerHandler);
-  const sunstar = new Sunstar(puppeteerHandler); 
+  const sunstar = new Sunstar(puppeteerHandler);
+  const abs = new Abscbn(puppeteerHandler);
+  const pia = new Pia(puppeteerHandler);
 
-  const scraped = [...(await inq.scrape()), ...(await he.scrape()), ...(await sunstar.scrape())];
+  const scraped = [
+    ...(await inq.scrape()),
+    ...(await he.scrape()),
+    ...(await sunstar.scrape()),
+    ...(await abs.scrape()),
+    ...(await pia.scrape()),
+  ];
   const supabase = new Supabase();
   await supabase.insertArticles(scraped);
   await puppeteerHandler.closeBrowser();
--- server/src/scraper/source/Pia.ts
+++ server/src/scraper/source/Pia.ts
@@ -4,23 +4,23 @@ import { extract } from "article-parser";
 import { Article } from "../article";
 import { Page } from "puppeteer";
 
-export class Abscbn extends Source implements Source {
+export class Pia extends Source implements Source {
   constructor(public puppeteerHandler: PuppeteerHandler) {
     super()
   }
 
-  name = "ABS CBN";
-  id = "5da76358-defe-42bc-8ada-252532b83180";
-  homepage = "https://news.abs-cbn.com/list/tag/baguio";
+  name = "Philippine Information Agency";
+  id = "edde96e9-2078-488e-8926-c4ecbd22f695";
+  homepage = "https://pia.gov.ph/news/regions/cordillera";
 
   getArticlesUrl = async () => {
     const handler = async (page: Page): Promise<string[]> => {
       try {
         page.goto(this.homepage, { waitUntil: "load", timeout: 0 });
-        await page.waitForSelector(".articles");
+        await page.waitForSelector(".list-articles");
         const links: string[] = await page.evaluate(() => {
           return Promise.resolve(
-            Array.from(document.querySelectorAll(".articles > article > a")).map(
+            Array.from(document.querySelectorAll(".media-body > h3 > a")).map(
               (link: Element) => (link as HTMLAnchorElement).href
             )
           );
