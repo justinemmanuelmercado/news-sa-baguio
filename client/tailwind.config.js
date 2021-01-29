7713c3243b371e6ee45963a4a07757c738e09f6c
EJ Mercado <justinemmanuelmercado@gmail.com>
2021-01-29 18:16:56 +0800

    chore: add custom tailwind config client side

---
+++ client/tailwind.config.js
@@ -0,0 +1,32 @@
+// tailwind.config.js
+/* eslint-env node */
+module.exports = {
+    theme: {
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            blue: {
+                500: '#5D848E',
+                400: '#68939E',
+                300: '#73A3B0',
+                200: '#80B5C3',
+                100: '#8EC9D9',
+            },
+            green: {
+                500: '#0BB68B',
+                400: '#23CFA4',
+                300: '#5ADCBB',
+                200: '#8EE9D2',
+                100: '#C4F2E7',
+            },
+            gray: {
+                600: '#2B2B2B',
+                500: '#545454',
+                400: '#686868',
+                300: '#CCCCCC',
+                200: '#F7F7F7',
+                100: '#FFFFFF',
+            },
+        },
+    },
+}
