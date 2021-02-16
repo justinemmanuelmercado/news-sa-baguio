b9a9e6f8d251ff94d99ea3e5396e734a3e410f3d
EJ Mercado <justinemmanuelmercado@gmail.com>
2021-02-16 16:34:34 +0800

    chore: remove wdyr

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,28 +5,16 @@ import { store } from './redux/store'
 import { Provider } from 'react-redux'
 import './index.css'
 
-function renderPage() {
-    const rootElement = document.getElementById('app')
+const rootElement = document.getElementById('app')
 
-    ReactDOM.render(
-        <React.StrictMode>
-            <Provider store={store}>
-                <App />
-            </Provider>
-        </React.StrictMode>,
-        rootElement,
-    )
-}
-
-async function catchRerendersInDev() {
-    // "monkey patches React to notify you [in console] about avoidable re-renders."
-    if (import.meta.env.MODE !== 'production') {
-        const whyDidYouRender = await import('@welldone-software/why-did-you-render')
-        whyDidYouRender.default(React, { include: [/App/] })
-    }
-}
-
-catchRerendersInDev().then(renderPage)
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>,
+    rootElement,
+)
 
 // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
 // Learn more: https://www.snowpack.dev/concepts/hot-module-replacement
