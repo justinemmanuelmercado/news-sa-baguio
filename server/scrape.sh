ad7537baa61b7b132e94d69cb92c4240264afdcd
EJ Mercado <justinemmanuelmercado@gmail.com>
2021-02-22 11:32:15 +0800

    chore: add scrape script

---
+++ server/scrape.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# paths can be relative to the current user that owns the crontab configuration
+
+# $(which node) returns the path to the current node version
+# either the one specified as `default` alias in NVM or a specific version set above
+# executing `nvm use 4 1> /dev/null` here won't work!
+$(which npm) run scrape;
