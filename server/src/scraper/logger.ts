fc9a3434bba0b11332fad167250fb6ea34073d31
EJ Mercado <justinemmanuelmercado@gmail.com>
2021-02-28 15:15:51 +0800

    chore: logging updates

--- server/src/scraper/logger.ts
+++ server/src/scraper/logger.ts
@@ -1,9 +1,11 @@
 import chalk from 'chalk'
 
 export const log = (message: string, topic = 'GENERAL', success = true): void => {
-    const prefix = success ? chalk.green.bold : chalk.red.bold
-    const msgMod = chalk.underline
+    const prefix = success ? chalk.green : chalk.red
+    const msgMod = chalk.underline.bold
     const status = success ? 'SUCCESS' : 'FAILED'
+    const currDate = new Date()
+    const dateString = `${currDate.getDate()}/${currDate.getMonth()} - ${currDate.getHours()}:${currDate.getSeconds()}`
 
-    console.log(`${prefix(`[${status}]`, `[${topic}]`, ':')} ${msgMod(message)}`)
+    console.log(`${prefix(`[${status}][${topic}][${dateString}]`)}: ${msgMod(message)} `)
 }
